301e3a876767f5e0d6d4e07e4867b3c93e656b28
niranjankumarnk <nkniranjankumar27@gmail.com>
2024-06-30 13:36:29 -0400

    First commit

---
+++ 1_chat_models/1_chat_model_basic.py
@@ -0,0 +1,20 @@
+# Chat Model Documents: https://python.langchain.com/v0.2/docs/integrations/chat/
+# OpenAI Chat Model Documents: https://python.langchain.com/v0.2/docs/integrations/chat/openai/
+
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+#Load environment variables from .env
+load_dotenv()
+
+#create a chatopenAI model
+
+model = ChatOpenAI(model = "gpt-3.5-turbo")
+model_1 = ChatGoogleGenerativeAI(model = "gemini-pro")
+
+#Invoke the model with message
+
+response = model_1.invoke("Write a ballad about LangChain")
+print("Full response: ", response)
+print("Content only: ", response.content)
